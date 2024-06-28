44d1e80ae0b5b7783ae1b53812abd287791089db
Xylo <82177322+itzxyloyt@users.noreply.github.com>
2024-06-28 05:23:15 -0700

    Update viewcount.php

--- viewcount.php
+++ viewcount.php
@@ -5,35 +5,23 @@ header("Last-Modified: $timestamp");
 header("Pragma: no-cache");
 header("Cache-Control: no-cache, must-revalidate");
 header("Content-type: image/svg+xml");
-function incrementFile($filename): int
-{
-    if (file_exists($filename)) {
-        $fp = fopen($filename, "r+") or die("Failed to open the file.");
+function incrementFile($filename): int {
+    if (file_exists($filename)) { $fp = fopen($filename, "r+") or die("Failed to open the file.");
         flock($fp, LOCK_EX);
         $count = fread($fp, filesize($filename)) + 1;
         ftruncate($fp, 0);
         fseek($fp, 0);
         fwrite($fp, $count);
         flock($fp, LOCK_UN);
-        fclose($fp);
-    }
-    else {
-        $count = 1;
-        file_put_contents($filename, $count);
-    }
-    return $count;
-}
-function shortNumber($num)
-{
+        fclose($fp); }
+    else { $count = 1;
+        file_put_contents($filename, $count); }
+    return $count; }
+function shortNumber($num) {
     $units = ['', 'K', 'M', 'B', 'T'];
-    for ($i = 0; $num >= 1000; $i++) {
-        $num /= 1000;
-    }
-    return round($num, 1) . $units[$i];
-}
-function curl_get_contents($url): string
-{
-    $ch = curl_init();
+    for ($i = 0; $num >= 1000; $i++) { $num /= 1000; }
+    return round($num, 1) . $units[$i]; }
+function curl_get_contents($url): string { $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -42,8 +30,7 @@ function curl_get_contents($url): string
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $response = curl_exec($ch);
     curl_close($ch);
-    return $response;
-}
+    return $response; }
 $message = incrementFile("views.txt");
 $params = [
     "label" => "Views",
@@ -54,4 +41,4 @@ $params = [
 ];
 $url = "https://img.shields.io/static/v1?" . http_build_query($params);
 echo curl_get_contents($url);
-?>
+?> 
